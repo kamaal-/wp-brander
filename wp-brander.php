1bd65d673e790451972d8109c0f6be7bfe3bfca6
Kamaal Aboothalib <kamaal@kamaal.me>
2013-11-18 14:57:42 +0400

    Plugin page
    Init
    
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- wp-brander.php
+++ wp-brander.php
@@ -1,2 +1,25 @@
 <?php
-?>
+/**
+ * WordPress Brander.
+ *
+ * A Wordpress plugin for custom favicon & custom admin login/register logo.
+ *
+ * @package   wp-brander
+ * @author    Kamaal Aboothalib <kamaal@kamaal.me>
+ * @license   GPL-2.0+
+ * @link      http://kamaal.me/wp-brander
+ * @copyright 2013 Kamaal Aboothalib
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WpBrander
+ * Plugin URI:        http://kamaal.me/wp-brander
+ * Description:       A Wordpress plugin for custom favicon & custom admin login/register logo.
+ * Version:           1.0.0.1
+ * Author:            Kamaal Aboothalib
+ * Author URI:        http://kamaal.me/
+ * Text Domain:       wp-brander-elephas
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/kamaal-/wp-brander
+ */

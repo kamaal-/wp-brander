d8c73d719f00a1b50d9faa69c884b5c76677d26d
Kamaal Aboothalib <kamaal@kamaal.me>
2013-11-26 14:47:09 +0400

    Endless Pointer
    Signed-off-by: Kamaal Aboothalib <kamaal@kamaal.me>

--- admin/assets/js/wp-brander-admin.js
+++ admin/assets/js/wp-brander-admin.js
@@ -1,5 +1,19 @@
 WpBranderAdmin = ( function () {
 
-	
+	console.log(pointerVars);
 
+	jQuery('.media-uploader-field').pointer({
+			content    : pointerVars.pointerObj,
+			position: {
+				'align' : 'midle',
+				'edge'  : 'top'
+			},
+			close: function(){
+				$.post( pointerVars.ajaxUrl, {
+                    pointer: pointer.pointer_id,
+                    action: 'dismiss-wp-pointer'
+                });
+			}
+			
+		}).pointer('open');
 }());
--- admin/class-wp-brander-admin.php
+++ admin/class-wp-brander-admin.php
@@ -8,7 +8,7 @@
  * @link      http://kamaal.me/wp-brander
  * @copyright 2013 Kamaal Aboothalib
  */
-
+ 
 /**
  * Singleton class. 
  * administrative side of the WordPress site.
@@ -18,7 +18,7 @@
  * @author  Kamaal Aboothalib <kamaal@kamaal.me>
  */
 class Wordpress_Brander_Admin{
-
+ 
     /**
      * Instance of this class.
      *
@@ -27,7 +27,7 @@ class Wordpress_Brander_Admin{
      * @var      object
      */
     protected static $instance = null;
-
+ 
     /**
      * Slug of the plugin screen.
      *
@@ -36,9 +36,9 @@ class Wordpress_Brander_Admin{
      * @var      string
      */
     protected $plugin_screen_hook_suffix = null;
-
+ 
     private $settings_api;
-
+ 
     /**
      * Initialize the plugin by loading admin scripts & styles and adding a
      * settings page and menu.
@@ -46,39 +46,39 @@ class Wordpress_Brander_Admin{
      * @since     1.0.0.1
      */
     private function __construct() {
-            
+             
             require_once(ABSPATH.'wp-admin/includes/plugin.php');
-
+ 
             $plugin = WP_Brander::get_instance();
-
+ 
             $this->settings_api = new Generate_Option( 'custom-menu_page_elephas-wp-brander' );
-
+ 
             $this->plugin_slug = $plugin->get_plugin_slug();
             $this->parent_slug = $plugin->get_parent_slug();
-
+ 
             // Load admin style sheet and JavaScript.
             add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_styles' ) );
             add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_admin_scripts' ) );
-
+ 
             // Add the options page and menu item.
             add_action( 'admin_menu', array( $this, 'add_plugin_admin_menu' ) );
-
-
-
+ 
+ 
+ 
             // Add an action link pointing to the options page.
             $plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . 'wp-brander.php' );
             add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
-
+ 
             //Action
             add_action( 'admin_init', array( $this, 'settings_api_init' ) );
-
+ 
             add_action('admin_enqueue_scripts', array( $this,'helper_pointers') );
-
+ 
             //Filter
             add_filter( '@TODO', array( $this, 'filter_method_name' ) );
-
+ 
     }
-
+ 
     /**
      * Return an instance of this class.
      *
@@ -87,7 +87,7 @@ class Wordpress_Brander_Admin{
      * @return    object    A single instance of this class.
      */
     public static function get_instance() {
-
+ 
             /*
              *
              * - Uncomment following lines if the admin class should only be available for super admins
@@ -95,15 +95,15 @@ class Wordpress_Brander_Admin{
             /* if( ! is_super_admin() ) {
                     return;
             } */
-
+ 
             // If the single instance hasn't been set, set it now.
             if ( null == self::$instance ) {
                     self::$instance = new self;
             }
-
+ 
             return self::$instance;
     }
-
+ 
     /**
      * Register and enqueue admin-specific style sheet.
      *
@@ -113,26 +113,26 @@ class Wordpress_Brander_Admin{
      * @return    null    Return early if no settings page is registered.
      */
     public function enqueue_admin_styles() {
-
+ 
             if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
                     return;
             }
-
+ 
             $screen = get_current_screen();
             if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-
+ 
                     //Load Latest media manager if exists
                     if( !function_exists('wp_enqueue_media') && version_compare( self::get_wordpress_version(), '3.5', '<=' ) ) {
                         wp_enqueue_style('thickbox');
                     }
-                    
-
+                     
+ 
                     wp_enqueue_style( 'wp-color-picker' );
                     wp_enqueue_style( $this->plugin_slug .'-admin-styles', plugins_url( 'assets/css/wp-brander-admin.css', __FILE__ ), array(), WP_Brander::VERSION );
             }
-
+ 
     }
-
+ 
     /**
      * Register and enqueue admin-specific JavaScript.
      *
@@ -142,42 +142,42 @@ class Wordpress_Brander_Admin{
      * @return    null    Return early if no settings page is registered.
      */
     public function enqueue_admin_scripts() {
-
+ 
             if ( ! isset( $this->plugin_screen_hook_suffix ) ) {
                     return;
             }
             wp_enqueue_script('wp-color-picker');
             $screen = get_current_screen();
             if ( $this->plugin_screen_hook_suffix == $screen->id ) {
-
+ 
                     //Load Latest media manager if exists
                     if( function_exists('wp_enqueue_media') && version_compare( self::get_wordpress_version(), '3.5', '>=' ) ) {
                         //call for new media manager
                         wp_enqueue_media();
-                        
+                         
                     }
                     //Or old WP < 3.5
                     else {
                         wp_enqueue_script('media-upload');
                         wp_enqueue_script('thickbox');
                     }
-
+ 
                     wp_enqueue_script( $this->plugin_slug . '-admin-script', plugins_url( 'assets/js/wp-brander-admin.js', __FILE__ ), array( 'jquery' ), WP_Brander::VERSION );
             }
-
+ 
     }
-
+ 
     /**
      * Register the administration menu for this plugin into the WordPress Dashboard menu.
      *
      * @since    1.0.0.1
      */
     public function add_plugin_admin_menu() {
-
+ 
             $topmenu_exists = $this->toplevel_menu_exists();
-
+ 
             if( !$topmenu_exists ){
-
+ 
                 add_menu_page(
                         __( 'Custom Settings', $this->plugin_slug ),
                         __( 'Custom menu', $this->parent_slug ),
@@ -185,7 +185,7 @@ class Wordpress_Brander_Admin{
                         $this->parent_slug,
                         array( $this, 'display_plugin_admin_page' )
                 );
-
+ 
                 $this->plugin_screen_hook_suffix = add_submenu_page( 
                         $this->parent_slug , 
                         'Wordpress Brander Settings', 
@@ -193,9 +193,9 @@ class Wordpress_Brander_Admin{
                         'remove_users', 
                         $this->plugin_slug, 
                         array( $this, 'display_plugin_admin_page' ) );
-
+ 
             }else{
-
+ 
                 $this->plugin_screen_hook_suffix = add_submenu_page( 
                         $this->parent_slug , 
                         'Wordpress Brander Settings', 
@@ -203,48 +203,48 @@ class Wordpress_Brander_Admin{
                         'remove_users', 
                         $this->plugin_slug, 
                         array( $this, 'display_plugin_admin_page' ) );
-
+ 
             }
-
+ 
     }
-
+ 
     /**
      * Render the settings page for this plugin.
      *
      * @since    1.0.0.1
      */
     public function display_plugin_admin_page() {
-
+ 
             echo '<div class="wrap">';
-
+ 
             $this->settings_api->show_navigation();
             $this->settings_api->show_forms();
-
+ 
             echo '</div>';
-
+ 
             $seoboosterpropluginfo = get_plugin_data( WP_PLUGIN_DIR . '/wp-brander/wp-brander.php');
             $version = $seoboosterpropluginfo['Version'];
-
+ 
             echo $version;
-            
+             
     }
-
+ 
     /**
      * Add settings action link to the plugins page.
      *
      * @since    1.0.0
      */
     public function add_action_links( $links ) {
-
+ 
             return array_merge(
                     array(
                             'settings' => '<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_slug ) . '">' . __( 'Settings', $this->plugin_slug ) . '</a>'
                     ),
                     $links
             );
-
+ 
     }
-
+ 
     /**
      *
      * @since    1.0.0.1
@@ -253,12 +253,12 @@ class Wordpress_Brander_Admin{
             //set the settings
             $this->settings_api->set_sections( $this->get_settings_sections() );
             $this->settings_api->set_fields( $this->get_settings_fields() );
-
+ 
             //initialize settings
             $this->settings_api->initialize();
     }
-
-
+ 
+ 
     function get_settings_sections() {
         $sections = array(
             array(
@@ -272,7 +272,7 @@ class Wordpress_Brander_Admin{
         );
         return $sections;
     }
-
+ 
     /**
      * Returns all the settings fields
      *
@@ -419,11 +419,11 @@ class Wordpress_Brander_Admin{
                 )
             )
         );
-
+ 
         return $settings_fields;
     }
-
-    
+ 
+     
     /**
      * NOTE:     Filters are points of execution in which WordPress modifies data
      *           before saving it or sending it to the browser.
@@ -440,37 +440,37 @@ class Wordpress_Brander_Admin{
     * Gettin wordpress version
     */
    protected function get_wordpress_version(){
-
+ 
             global $wp_version;
-
+ 
             return $wp_version;
-
+ 
     }
-
+ 
     /**
     * Check admin top level menu exists
     */
     protected function toplevel_menu_exists(){
-
+ 
             global $menu;
-
+ 
             $menu_exist = false;
-
+ 
             foreach($menu as $item) {
                 if(strtolower($item[0]) == strtolower('Custom menu')) {
                     $menu_exist = true;
                 }
             }
-
+ 
             return $menu_exist;
-
+ 
     }
-
+ 
     public function helper_pointers(){
-
+ 
         $pointers = array(
                         array(
-                            'id' => '65894',   // unique id for this pointer
+                            'id' => '54',   // unique id for this pointer
                             'screen' => 'custom-menu_page_elephas-wp-brander', // this is the page hook we want our pointer to show on
                             'target' => '.media-uploader-field', // the css selector for the pointer to be tied to, best to use ID's
                             'title' => 'My ToolTip',
@@ -479,24 +479,11 @@ class Wordpress_Brander_Admin{
                                                'edge' => 'top', //top, bottom, left, right
                                                'align' => 'middle' //top, bottom, left, right, middle
                                                )
-                            ),
-
-                        array(
-                            'id' => '6581',
-                            'screen' => 'custom-menu_page_elephas-wp-brander',
-                             'target' => '.regular-text',
-                             'title' => 'Regular Text',
-                            'content' => 'Description',
-                            'position' => array( 
-                                   'edge' => 'left', //top, bottom, left, right
-                                   'align' => 'right' //top, bottom, left, right, middle
-                                   )
-                        )
-                         // more as needed
+                            )
                          );
        //Now we instantiate the class and pass our pointer array to the constructor 
        $myPointers = new WP_Help_Pointer($pointers);
-
+ 
     }
-
+ 
 }
